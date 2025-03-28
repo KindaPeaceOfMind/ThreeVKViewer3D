6825a819bb28026eaa92ad83b35cbe6c5d3aa5da
KindaPeaceOfMind <94527259+KindaPeaceOfMind@users.noreply.github.com>
2025-03-28 12:58:05 +1000

    Update init.js

--- Graph/init.js
+++ Graph/init.js
@@ -1,5 +1,5 @@
 let target = '';
-
+let token_key = '';  
 init()
 function init() {
 
@@ -17,7 +17,8 @@ function init() {
     
   ].forEach((el)=>AddScript(el));
 
-  target = prompt('Write userId');
+  target = prompt('Напишите Id пользователя, друзей которого нужно вывести');
+  token_key = '6e7b4f086e7b4f086e7b4f08016d60135566e7b6e7b4f0808cd415a2b0d6aabb97b6119'
 }
 
 function AddScript(src) {
